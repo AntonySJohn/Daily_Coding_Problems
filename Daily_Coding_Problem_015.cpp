1197189bd16db65744a2ee7ca963dbfdda4eddf9
Tim Kante <timkante99@web.de>
2019-10-06 01:40:00 +0200

    Finished problem #015

--- Daily_Coding_Problem_015.cpp
+++ Daily_Coding_Problem_015.cpp
@@ -4,6 +4,23 @@ Given a stream of elements too large to store in memory,
 pick a random element from the stream with uniform probability.
 */
 
+#include <iostream>
+#include <random>
+#include <vector>
+
+int reservoirSampling(const std::vector<int>& stream){
+    int index(1);
+    int currentSample = 0;
+    for (auto& element : stream){
+        if (rand() % index++ == 0) currentSample = element;
+    }
+    return currentSample;
+}
+
 int main(){
+    std::vector<int> exanmple{0,1,2,3,4,5,6,7,8,9,10,11};
+    for (int i = 0; i < 5; i++){
+        std::cout << reservoirSampling(exanmple) << std::endl;
+    }
     return 0;
 }
