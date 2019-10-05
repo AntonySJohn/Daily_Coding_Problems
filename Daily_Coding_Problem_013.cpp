8fd92fb13fcbe8ee59d6b99516afcf60682b6ec2
Tim Kante <timkante99@web.de>
2019-10-05 21:22:34 +0200

    Finished Problem #013

--- Daily_Coding_Problem_013.cpp
+++ Daily_Coding_Problem_013.cpp
@@ -7,6 +7,56 @@ For example, given s = "abcba" and k = 2, the longest
 substring with k distinct characters is "bcb".
 */
 
+#include <algorithm>
+#include <cassert>
+#include <vector>
+#include <string>
+
+auto longestSubstring(int distinct, const std::string& str){
+    std::vector<std::pair<char, int>> distChars;
+    int maxSubsting = 0;
+
+    //find longest substring for every char in the string
+    //as begin point of the search
+    for (auto& ch : str){
+        distChars.push_back({ch, 1});
+        auto itr = &ch;
+
+        //loop to the end of the string from the current char onwards
+        while (*(itr++)){
+
+            //when the char is not in the set of already seen chars
+            //(if the char is in the seen ones, increment the chars counter)
+            if (std::find_if(distChars.begin(), distChars.end(), 
+                [itr](std::pair<char,int>& p) 
+                    {   if (p.first == *itr) {
+                            p.second++;
+                            return true;
+                        } else return false;
+                    }) == distChars.end()){
+                
+                //maximum distinct integers found
+                if (distChars.size() == distinct){
+                    int temp = 0;
+                    for (auto& p : distChars) temp += p.second;
+                    maxSubsting = std::max(maxSubsting, temp);
+                    break;
+
+                //we have distinct char left to add
+                } else {
+                    distChars.push_back({*itr, 1});
+                }
+            }
+        }
+        distChars.clear();
+    }
+
+    return maxSubsting;
+}
+
 int main(){
+    assert(longestSubstring(2, "abcba") == 3);
+    assert(longestSubstring(8, "") == 0);
+    assert(longestSubstring(3, "aaaabcdeffffff") == 8);
     return 0;
 }
