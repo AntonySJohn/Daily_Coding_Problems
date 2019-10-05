6ff3a074f4e68c72cd3848a1b63e525f2002a736
Tim Kante <timkante99@web.de>
2019-10-06 01:19:07 +0200

    Finished Problem #014

--- Daily_Coding_Problem_014.cpp
+++ Daily_Coding_Problem_014.cpp
@@ -6,6 +6,29 @@ Estimate π to 3 decimal places using a Monte Carlo method.
 Hint: The basic equation of a circle is x² + y² = r².
 */
 
+#include <cassert>
+#include <random>
+
+double getPi(){
+    double testPoints(10'000'000), inside(0), x(0), y(0);
+
+    std::random_device seed;
+    std::mt19937 randomNumber(seed());
+    std::uniform_real_distribution<> uni(-0.5, 0.5);
+
+    auto circleTest = [](double x, double y){
+        return x*x + y*y <= 0.5*0.5;
+    };
+
+    for (int i = 0; i < testPoints; i++){
+        x = uni(randomNumber);
+        y = uni(randomNumber);
+        if (circleTest(x,y)) inside++;
+    }
+
+    return (4 * inside) / testPoints;
+}
+
 int main(){
-    return 0;
+    assert(fabs(getPi() - 3.141f) <= 0.001f);
 }
