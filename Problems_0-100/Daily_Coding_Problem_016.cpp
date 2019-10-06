0c92798ea7385c34c1c820f337626cea74dcdeea
Tim Kante <timkante99@web.de>
2019-10-06 16:20:45 +0200

    Finished Problem #016

--- Problems_0-100/Daily_Coding_Problem_016.cpp
+++ Problems_0-100/Daily_Coding_Problem_016.cpp
@@ -12,6 +12,39 @@ get_last(i): gets the ith last element from the log.
 You should be as efficient with time and space as possible.
 */
 
+#include <algorithm>
+#include <cassert>
+#include <list>
+
+class log{
+private:
+    const int n = 5;
+    std::list<int> orderIDs;
+public:
+    log(int size) : n(size) {}
+    auto record(int orderID){
+        if (this->orderIDs.size() >= this->n)
+            this->orderIDs.pop_back();
+        this->orderIDs.emplace_front(orderID);
+    }
+    auto get_last(int i) const{
+        assert(i > 0);
+	    std::list<int>::const_iterator it = this->orderIDs.begin();
+	    std::advance(it, i-1);
+        return *it;
+    }
+};
+
 int main(){
+    log example(4);
+    example.record(2);
+    example.record(4);
+    example.record(6);
+    example.record(8);
+    example.record(10);
+    example.record(12);
+    auto a = example.get_last(1);
+    auto b = example.get_last(4);
+    assert(a == 12 && b == 6);
     return 0;
 }
