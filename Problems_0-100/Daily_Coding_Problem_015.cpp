8c9341a21fe717a11a69b6eb143f02fb2481d45b
Tim Kante <timkante99@web.de>
2019-10-06 13:55:23 +0200

    Replaced loop with for_each-algorithm

--- Problems_0-100/Daily_Coding_Problem_015.cpp
+++ Problems_0-100/Daily_Coding_Problem_015.cpp
@@ -4,21 +4,23 @@ Given a stream of elements too large to store in memory,
 pick a random element from the stream with uniform probability.
 */
 
+#include <algorithm>
 #include <iostream>
 #include <random>
 #include <vector>
 
-int reservoirSampling(const std::vector<int>& stream){
-    int index(1);
-    int currentSample = 0;
-    for (auto& element : stream){
-        if (rand() % index++ == 0) currentSample = element;
-    }
+int reservoirSampling(const std::vector<int> &stream){
+    int index(1), currentSample = 0;
+    std::for_each(stream.begin(), stream.end(),
+                  [&index, &currentSample](const auto &element) {
+                      if (rand() % index++ == 0)
+                          currentSample = element;
+                  });
     return currentSample;
 }
 
 int main(){
-    std::vector<int> exanmple{0,1,2,3,4,5,6,7,8,9,10,11};
+    std::vector<int> exanmple{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
     for (int i = 0; i < 5; i++){
         std::cout << reservoirSampling(exanmple) << std::endl;
     }
