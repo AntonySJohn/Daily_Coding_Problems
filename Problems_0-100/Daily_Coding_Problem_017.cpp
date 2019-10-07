8c7b80f42674ac65000b5646e08d60250ec998ac
Tim Kante <timkante99@web.de>
2019-10-07 11:35:48 +0200

    Reformated Code

--- Problems_0-100/Daily_Coding_Problem_017.cpp
+++ Problems_0-100/Daily_Coding_Problem_017.cpp
@@ -8,7 +8,7 @@ dir
     subdir2
         file.ext
 
-The directory dir contains an empty sub-directory subdir1 
+The directory dir contains an empty sub-directory subdir1
 and a sub-directory subdir2 containing a file file.ext.
 
 The string "dir\n\tsubdir1\n\t\tfile1.ext\n
@@ -22,16 +22,16 @@ dir
         subsubdir2
             file2.ext
 
-The directory dir contains two sub-directories subdir1 and subdir2. 
-subdir1 contains a file file1.ext and an empty second-level sub-directory 
-subsubdir1. subdir2 contains a second-level sub-directory subsubdir2 
+The directory dir contains two sub-directories subdir1 and subdir2.
+subdir1 contains a file file1.ext and an empty second-level sub-directory
+subsubdir1. subdir2 contains a second-level sub-directory subsubdir2
 containing a file file2.ext.
-We are interested in finding the longest (number of characters) absolute 
-path to a file within our file system. For example, in the second example 
-above, the longest absolute path is "dir/subdir2/subsubdir2/file2.ext", 
+We are interested in finding the longest (number of characters) absolute
+path to a file within our file system. For example, in the second example
+above, the longest absolute path is "dir/subdir2/subsubdir2/file2.ext",
 and its length is 32 (not including the double quotes).
-Given a string representing the file system in the above format, return 
-the length of the longest absolute path to a file in the abstracted file 
+Given a string representing the file system in the above format, return
+the length of the longest absolute path to a file in the abstracted file
 system. If there is no file in the system, return 0.
 
 Note:
@@ -40,28 +40,29 @@ The name of a directory or sub-directory will not contain a period.
 */
 
 #include <algorithm>
-#include <iostream>
 #include <cassert>
+#include <iostream>
 #include <sstream>
 #include <string>
 #include <vector>
 
-class directoryTree{
+class directoryTree {
 private:
     int level;
     bool file;
     std::string dirName;
     directoryTree* parentDir;
     std::vector<directoryTree*> subdirectorys;
-public:
-    directoryTree() : level(-1), file(false), dirName("*"), parentDir(nullptr){}
-    directoryTree(int level, const std::string& dirName, bool isFile, directoryTree* parent) :
-        level(level), dirName(dirName), file(isFile), parentDir(parent) {}
 
-    auto parent(){return this->parentDir;}
-    auto add(directoryTree* newSubdir){this->subdirectorys.push_back(newSubdir);}
-    static auto generateTree(const std::string& path){
-        auto split = [](const std::string& str, char delimiter){
+public:
+    directoryTree() : level(-1), file(false), dirName("*"), parentDir(nullptr) {}
+    directoryTree(int level, const std::string& dirName, bool isFile, directoryTree* parent)
+        : level(level), dirName(dirName), file(isFile), parentDir(parent) {}
+
+    auto parent() { return this->parentDir; }
+    auto add(directoryTree* newSubdir) { this->subdirectorys.push_back(newSubdir); }
+    static auto generateTree(const std::string& path) {
+        auto split = [](const std::string& str, char delimiter) {
             std::vector<std::string> tokens;
             std::string token;
             std::istringstream tokenStream(str);
@@ -75,22 +76,21 @@ public:
         directoryTree* nextNode = nullptr;
         int currentLevel = -1;
 
-        std::for_each(tokens.begin(), tokens.end(), 
-            [&](const auto& token){
-                int tab(0);
-                while (token.at(tab) == '\t') tab++;
-                if (currentLevel >= tab){
-                    int i = currentLevel - tab + 1;
-                    while (i--) newTree = newTree->parent();
-                }
-                if (split(token, '.').size() > 1) 
-                    nextNode = new directoryTree(tab, token.substr(tab), true, newTree);
-                else nextNode = new directoryTree(tab, token.substr(tab), false, newTree);
-                newTree->add(nextNode);
-                currentLevel = tab;
-                newTree = nextNode;
+        std::for_each(tokens.begin(), tokens.end(), [&](const auto& token) {
+            int tab(0);
+            while (token.at(tab) == '\t') tab++;
+            if (currentLevel >= tab) {
+                int i = currentLevel - tab + 1;
+                while (i--) newTree = newTree->parent();
             }
-        );
+            if (split(token, '.').size() > 1)
+                nextNode = new directoryTree(tab, token.substr(tab), true, newTree);
+            else
+                nextNode = new directoryTree(tab, token.substr(tab), false, newTree);
+            newTree->add(nextNode);
+            currentLevel = tab;
+            newTree = nextNode;
+        });
         return returnTree;
     }
 
@@ -99,17 +99,20 @@ public:
         if (this->subdirectorys.size() == 0) return 0;
         int maxLength(0);
         std::for_each(this->subdirectorys.begin(), this->subdirectorys.end(),
-            [&maxLength](const auto& subdir){
-                maxLength = std::max(maxLength, subdir->longestFilepath());
-            }
-        );
+                      [&maxLength](const auto& subdir) {
+                          maxLength = std::max(maxLength, subdir->longestFilepath());
+                      });
         return this->level < 0 ? maxLength : maxLength + this->dirName.size() + 1;
     }
-    ~directoryTree(){for (auto& node : this->subdirectorys) delete node; delete this;}
+    ~directoryTree() {
+        for (auto& node : this->subdirectorys) delete node;
+        delete this;
+    }
 };
 
-int main(){
-    std::string example = "dir\n\tsubdir1\n\t\tfile1.ext\n"\
+int main() {
+    std::string example =
+        "dir\n\tsubdir1\n\t\tfile1.ext\n"
         "\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext";
     auto exampleTree = directoryTree::generateTree(example);
     assert(exampleTree->longestFilepath() == 32);
